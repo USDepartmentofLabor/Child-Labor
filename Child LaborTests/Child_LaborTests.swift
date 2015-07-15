1d9dcb35e17d42ebf700f543ecf1ec384fe9500c
Michael Pulsifer <pulsifer.michael@dol.gov>
2015-07-15 16:00:39 -0400

    Updated copyright notice to one that is appropriate.

--- Child LaborTests/Child_LaborTests.swift
+++ Child LaborTests/Child_LaborTests.swift
@@ -3,7 +3,7 @@
 //  Child LaborTests
 //
 //  Created by E J Kalafarski on 3/2/15.
-//  Copyright (c) 2015 E J Kalafarski. All rights reserved.
+//  U.S. Government Work https://www.usa.gov/government-works
 //
 
 import UIKit
