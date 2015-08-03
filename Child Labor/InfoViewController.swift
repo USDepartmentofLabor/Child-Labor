40dec918c957647cf4ef6b64753b38790fb735ae
skeeJay <e.j.kalafarski@gmail.com>
2015-08-03 00:33:34 -0400

    About and methodology webviews

---
+++ Child Labor/InfoViewController.swift
@@ -0,0 +1,72 @@
+//
+//  InfoViewController.swift
+//  Child Labor
+//
+//  Created by E J Kalafarski on 7/31/15.
+//  Copyright © 2015 U.S. Department of Labor. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController, UIWebViewDelegate {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    var infoContent = "aboutthisapp"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        webView.delegate = self
+        
+        // Set navigation bar title
+        if infoContent == "aboutthisapp" {
+            self.title = "About This App"
+        } else if infoContent == "methodology" {
+            self.title = "Methodology"
+        }
+        
+        // Get the contents of the file to load
+        let localFilePath = NSBundle.mainBundle().pathForResource(infoContent, ofType: "html")
+        var contents: NSString?
+        do {
+            contents = try NSString(contentsOfFile: localFilePath!, encoding: NSUTF8StringEncoding)
+        } catch _ {
+            contents = nil
+        }
+        
+        // Get the base URL of the file so we can access its resources
+        let baseUrl = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath)
+        
+        // Load contents into the webview
+        webView.loadHTMLString(contents as! String, baseURL: baseUrl)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        // If it's a web link
+        if request.URL!.scheme == "http" {
+            UIApplication.sharedApplication().openURL(request.URL!)
+            return false
+        }
+        
+        // If it's the initial load
+        return true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Child Labor/MoreInfoController.swift
@@ -0,0 +1,108 @@
+//
+//  MoreInfoController.swift
+//  Child Labor
+//
+//  Created by E J Kalafarski on 7/31/15.
+//  Copyright © 2015 U.S. Department of Labor. All rights reserved.
+//
+
+import UIKit
+
+class MoreInfoController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Make sure the ugly table cell selection is cleared when returning to this view
+        if let tableIndex = self.tableView.indexPathForSelectedRow {
+            self.tableView.deselectRowAtIndexPath(tableIndex, animated: false)
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    // Allow more info view to be closed
+    @IBAction func dismissMoreInfo(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // Load content for the selected web view
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "presentAboutThisApp" {
+            let destinationViewController = segue.destinationViewController as! InfoViewController
+            destinationViewController.infoContent = "aboutthisapp"
+        } else if segue.identifier == "presentMethodology" {
+            let destinationViewController = segue.destinationViewController as! InfoViewController
+            destinationViewController.infoContent = "methodology"
+        }
+    }
+
+}
