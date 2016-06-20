b554e61dae7e3528c452de18b47f1efb2a3d629f
Trumaine Johnson <trumaine.johnson@aquilent.com>
2016-06-20 11:22:30 -0400

    Updated Enforcement headings

--- Child Labor/EnforceTableViewController.swift
+++ Child Labor/EnforceTableViewController.swift
@@ -44,7 +44,7 @@ class EnforceTableViewController: UITableViewController {
         if state == 0 {
             switch section {
             case 0:
-                return 3
+                return 4
             case 1:
                 return 3
             case 2:
